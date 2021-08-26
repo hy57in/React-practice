587c038aebcf4c09aaf1d05ea074696c631f9e26
HyojinKim <gywls00100@gmail.com>
2021-08-26 16:00:51 +0900

    onMouseOut

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -1,6 +1,6 @@
 // src폴더안에 App.js을 연 다음 기존 내용을 모두 지우고 아래의 내용으로 작성해 보세요
 import React from 'react';
-import Counter from './03/Counter2';
+import Counter3 from './03/Counter3';
 
 class App extends React.Component {
   constructor(props) {
@@ -15,7 +15,7 @@ class App extends React.Component {
     this.setState(({ count }) => ({ count: count + 1 }));
   }
   render() {
-    return <Counter count={this.state.count} onAdd={this.increateCount} />;
+    return <Counter3 count={this.state.count} onAdd={this.increateCount} />;
   }
 }
 
