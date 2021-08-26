a6d95ed97d912f8e706857daeded06f2166ab070
HyojinKim <gywls00100@gmail.com>
2021-08-26 14:58:53 +0900

    배열 컴포넌트

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -1,26 +1,11 @@
 import React from 'react';
-import Counter from './03/Counter';
-import NewCounter from './03/NewCounter';
+import ListExample from './03/ListExample';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { count: 0 };
-    this.resetCount = this.resetCount.bind(this);
-  }
-  resetCount() {
-    this.setState(({ count }) => ({ count: count + 10 }));
-  }
   render() {
     return (
       <div>
-        <div>
-          <Counter count={this.state.count} />
-        </div>
-        <div>
-          <NewCounter count={this.state.count} />
-        </div>
-        <button onClick={this.resetCount}>{this.state.count + 10}으로 초기화</button>
+        <ListExample />
       </div>
     );
   }
